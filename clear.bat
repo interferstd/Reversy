e68537140448597919c7b8b9e5503f32b401ef19
Дмитрий Бычков <dimac090501@hotmail.com>
2019-03-29 23:43:42 +0300

    inited v0.0.0.1

---
+++ clear.bat
@@ -0,0 +1,4 @@
+@echo off
+set /p str=Enter mask:
+set /p check=Shure?(y/n)
+if %check% == y @ 2>nul (@for /f "delims=" %%j in ('dir/a-d/b/s "%str%"') do @rm "%%j")
---
+++ clone.bat
@@ -0,0 +1,3 @@
+@echo off
+set /p str=Enter repository name:
+git clone https://github.com/interferstd/%str%.git
